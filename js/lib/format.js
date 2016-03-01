90e29581d64df40a8687508b0853154d793dd5fd
Shea Bunge <info@bungeshea.com>
2016-03-01 21:32:22 +1100

    Allow decimal points in base 10 number

--- js/lib/format.js
+++ js/lib/format.js
@@ -1,27 +1,5 @@
 
 /**
- * Separate a number into columns using spaces
- * @param {string} num The original number
- * @param {number} col The amount of digits per column
- * @returns {string} The formatted number
- */
-var columnize = function (num, col) {
-	num = num + '';
-	var before = num.split('').reverse();
-	var after = [];
-
-	for (var i = 0; i < before.length; i++) {
-		after.push(before[i]);
-
-		if ((i+1) % col === 0) {
-			after.push(' ');
-		}
-	}
-
-	return after.reverse().join('').trim();
-};
-
-/**
  * Format a decimal number for display.
  * Adds column separators and removes leading zeros.
  *
@@ -38,13 +16,11 @@ var format_dec = function (dec) {
 	// remove leading zeros
 	dec = dec.replace(/^0+([1-9])/, '$1');
 
-	// separate into columns
+	// add columns
 	var padding = 3 - dec.length % 3;
 	if (padding == 3) padding = 0;
 	dec = ' '.repeat(padding) + dec;
-	dec = dec.replace(/(.{3})/g, '$1 ').trim();
-
-	return dec;
+	return dec.replace(/(\d{3}[^.])/g, '$1 ').trim();
 };
 
 /**
@@ -73,4 +49,27 @@ var format_bin = function (bin, bits) {
 
 	bin = '0'.repeat(padding) + bin;
 	return bin.replace(/(\d{4})/g, '$1 ').trim();
+};
+
+
+/**
+ * Separate a number into columns using spaces
+ * @param {string} num The original number
+ * @param {number} col The amount of digits per column
+ * @returns {string} The formatted number
+ */
+var columnize = function (num, col) {
+	num = num + '';
+	var before = num.split('').reverse();
+	var after = [];
+
+	for (var i = 0; i < before.length; i++) {
+		after.push(before[i]);
+
+		if ((i+1) % col === 0) {
+			after.push(' ');
+		}
+	}
+
+	return after.reverse().join('').trim();
 };
