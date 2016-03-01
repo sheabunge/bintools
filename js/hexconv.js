8fb70467b70135dc05c85aaf58f525311fa4d482
Shea Bunge <info@bungeshea.com>
2016-03-01 13:04:31 +1100

    Update hex converter to use new functions

--- js/hexconv.js
+++ js/hexconv.js
@@ -2,34 +2,20 @@ var hex_input = document.querySelector('[name=hex]');
 var dec_input = document.querySelector('[name=dec]');
 var bin_input = document.querySelector('[name=bin]');
 
-var update_dec = function (dec) {
+var update = function (dec) {
 	dec_input.value = columnize(dec, 3);
+	hex_input.value = (dec * 1).toString(16);
+	bin_input.value = format_bin(dec2bin(dec));
 };
-var update_hex = function (dec) {
-	hex_input.value = dec.toString(16);
-};
-var update_bin = function (dec) {
-	bin_input.value = columnize(dec2bin(dec), 4);
-};
-
-update_dec(0);
-update_hex(0);
-update_bin(0);
 
 hex_input.oninput = function () {
-	var dec = parseInt(this.value, 16);
-	update_dec(dec);
-	update_bin(dec);
+	update(parseInt(this.value, 16));
 };
 
 dec_input.oninput = function () {
-	var dec = this.value * 1;
-	update_hex(dec);
-	update_bin(dec);
+	update(this.value.replace(/\D/g, ''));
 };
 
 bin_input.oninput = function () {
-	var dec = bin2dec(this.value);
-	update_hex(dec);
-	update_dec(dec);
+	update(bin2dec(this.value));
 };
