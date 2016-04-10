7b71b48615272ff5821cf89719bea5a0e05b3075
Shea Bunge <info@bungeshea.com>
2016-04-10 18:01:57 +1000

    Add conversion of floating point decimal to binary

--- js/float.js
+++ js/float.js
@@ -1,59 +1,117 @@
+(function () {
+    var binary_input = document.getElementById('binary');
+    var decimal_input = document.getElementById('decimal');
 
-var binary_input = document.getElementById('binary');
-var decimal_input = document.getElementById('decimal');
+    var bits = {
+        sign: 1,
+        exp: 3,
+        mantissa: 5
+    };
 
-var bits = {
-    sign: 1,
-    exp: 3,
-    mantissa: 5
-};
+    /**
+     * Pad a string with a leading character so it
+     * is a fixed length
+     * @param {string|number} s String to pad
+     * @param {number} length Desired length of string
+     * @param {string} [char] Character pad with
+     * @returns {string}
+     */
+    var pad = function (s, length, char) {
+        s += '';
+        char = char || '0';
 
-binary_input.oninput = function () {
-    var bin = this.value.replace(/[^01]/g, '');
+        while (s.length < length) {
+            s = char + s;
+        }
 
-    if (bin.length > bits.sign + bits.exp + bits.mantissa) {
-        decimal_input.value = 'too many bits';
-        return;
-    }
+        return s;
+    };
 
-    if (bin.length < bits.sign + bits.exp + bits.mantissa) {
-        decimal_input.value = 'not enough bits';
-        return;
-    }
+    binary_input.oninput = function () {
+        var bin = this.value.replace(/[^01]/g, '');
 
-    var sign = bin.substr(0, bits.sign);
-    var exp = bin.substr(bits.sign, bits.exp);
-    var mantissa = bin.substr(bits.sign + bits.exp, bits.mantissa);
+        if (bin.length > bits.sign + bits.exp + bits.mantissa) {
+            decimal_input.value = 'too many bits';
+            return;
+        }
 
-    console.log('sign: ' + sign);
+        if (bin.length < bits.sign + bits.exp + bits.mantissa) {
+            decimal_input.value = 'not enough bits';
+            return;
+        }
 
-    // Convert exponent to two's complement decimal
-    console.log('exp: ' + exp);
-    if (exp[0] === '1') {
-        exp = bin2dec(twoscomp(exp)) * -1;
-    } else {
-        exp = bin2dec(exp);
-    }
-    console.log('exp: ' + exp);
+        var sign = bin.substr(0, bits.sign);
+        var exp = bin.substr(bits.sign, bits.exp);
+        var mantissa = bin.substr(bits.sign + bits.exp, bits.mantissa);
 
-    // Convert mantissa to decimal
-    mantissa /= Math.pow(10, mantissa.length);
-    console.log('mantissa: ' + mantissa);
-    mantissa = bin2dec(mantissa + '');
-    console.log('mantissa: ' + mantissa);
+        console.log('sign: ' + sign);
 
-    // Calculate decimal number
-    var dec = mantissa * Math.pow(2, exp*1);
+        // Convert exponent to two's complement decimal
+        console.log('exp: ' + exp);
+        if (exp[0] === '1') {
+            exp = bin2dec(twoscomp(exp)) * -1;
+        } else {
+            exp = bin2dec(exp);
+        }
+        console.log('exp: ' + exp);
 
-    if (sign === '1') {
-        dec *= -1;
-    }
+        // Convert mantissa to decimal
+        mantissa /= Math.pow(10, mantissa.length);
+        console.log('mantissa: ' + mantissa);
+        mantissa = bin2dec(mantissa + '');
+        console.log('mantissa: ' + mantissa);
 
-    decimal_input.value = dec;
-    console.log();
-};
+        // Calculate decimal number
+        var dec = mantissa * Math.pow(2, exp*1);
 
-decimal_input.oninput = function () {
-    var dec = this.value.replace(/[^\d.]/g, '');
+        if (sign === '1') {
+            dec *= -1;
+        }
 
-};
+        decimal_input.value = dec;
+        console.log();
+    };
+
+    decimal_input.oninput = function () {
+        var dec = this.value.replace(/[^\d.\-+]/g, '') * 1;
+
+        // calculate sign
+        var sign = '0';
+        if (dec < 0) {
+            sign = '1';
+            dec *= -1;
+        }
+
+        // convert to binary
+        var exp = 0;
+        var mantissa = dec2bin(dec);
+
+        // normalise mantissa
+        if (mantissa < 1) {
+            mantissa = mantissa.replace(/^0./, '');
+            for (var i = 0; mantissa[i] == '0'; ++i, --exp) {}
+            mantissa = mantissa.substr(i);
+        } else {
+            // calculate exponent
+            var int = (Math.floor(mantissa * 1) + '');
+            exp = '0' == int ? 0 : int.length;
+            mantissa = mantissa.replace(/^0+|0+$|\./g, '');
+        }
+
+        // convert to required size
+        mantissa = mantissa.substr(0, bits.mantissa);
+        mantissa += '0'.repeat(bits.mantissa - mantissa.length);
+
+        // convert exponent to binary
+        if (exp < 0) {
+            exp = dec2bin(-exp);
+            exp = twoscomp(pad(exp, bits.exp));
+        } else {
+            exp = pad(dec2bin(exp), bits.exp);
+        }
+
+        // display result
+        binary_input.value = sign + ' ' + exp + ' ' + mantissa;
+    };
+
+})();
